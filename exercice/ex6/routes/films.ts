0233ce466e98bd00fe0e1f06e0c5d428ae5944ed
Keyvan <keyvan.nikazm@gmail.com>
2024-10-04 10:49:57 +0200

    new: ex1.6

--- exercice/ex6/routes/films.ts
+++ exercice/ex6/routes/films.ts
@@ -86,7 +86,7 @@ router.post("/", (req, res) => {
         (film) =>
         (film.title.toLowerCase() === NewFilm.title.toLowerCase()) &&
         (film.director.toLowerCase() === NewFilm.director.toLowerCase())
-    )
+    );
 
     if (exists) {
         return res.status(409).json({error: "Film already exists"});
@@ -95,4 +95,116 @@ router.post("/", (req, res) => {
     films.push(NewFilm);
     return res.json(NewFilm);
     });
+
+router.delete("/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const index = films.findIndex((film) => film.id === id);
+    if (index === -1) {
+        return res.sendStatus(404).json({error: "Film not found"});
+    }
+    const deleteElements = films.splice(index, 1);
+    return res.json(deleteElements[0]);
+});
+
+router.patch("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const index = films.findIndex((film) => film.id === id);
+    if (!index) {
+        return res.status(404);
+    }
+    const body: unknown = req.body;
+
+    if (!body || 
+        typeof body !== "object" ||
+        ("title" in body && (typeof body.title !== "string" || !body.title.trim())) ||
+        ("director" in body && (typeof body.director !== "string" || !body.director.trim())) ||
+        ("duration" in body && (typeof body.duration !== "number" || body.duration <= 0)) ||
+        ("budget" in body && (typeof body.budget !== "number" || body.budget <= 0)) ||
+        ("description" in body && (typeof body.description !== "string")) ||
+        ("imageUrl" in body && (typeof body.imageUrl !== "string"))
+    ) {
+        return res.sendStatus(400).json({error: "Invalid body"});
+    }
+
+    const { title, director, duration, budget, description, imageUrl }: Partial<NewFilm> = body;
+
+    if (title) {
+        films[index].title = title;
+    }
+    if (director) {
+        films[index].director = director;
+    }
+    if (duration) {
+        films[index].duration = duration;
+    }
+    if (budget) {
+        films[index].budget = budget;
+    }
+    if (description) {
+        films[index].description = description;
+    }
+    if (imageUrl) {
+        films[index].imageUrl = imageUrl;
+    }
+
+    return res.json(films[index]);
+});
+
+// faire le router.put
+
+router.put("/:id", (req, res) => {
+    const body: unknown = req.body;
+    
+    if (!body || 
+        typeof body !== "object" ||
+        !("title" in body) ||
+        !("director" in body) ||
+        !("duration" in body) ||
+        typeof body.title !== "string" ||
+        typeof body.director !== "string" ||
+        typeof body.duration !== "number" ||
+        !body.title.trim() ||
+        !body.director.trim() ||
+        body.duration <= 0 ||
+        (("budget" in body) && (typeof body.budget !== "number" || body.budget <= 0)) ||
+        (("description" in body) && (typeof body.description !== "string")) ||
+        (("imageUrl" in body) && (typeof body.imageUrl !== "string"))
+    ) {
+        return res.sendStatus(400).json({error: "Invalid body"});
+    }
+
+    const id = Number(req.params.id);
+    if(!id){
+        return res.status(400);
+    }
+    const index = films.findIndex(films => films.id === id);
+    if(index < 0){
+        const NewFilm = body as NewFilm;
+    
+        const exists = films.find(
+            (film) =>
+            (film.title.toLowerCase() === NewFilm.title.toLowerCase()) &&
+            (film.director.toLowerCase() === NewFilm.director.toLowerCase())
+        );
+
+    if (exists) {
+        return res.status(409).json({error: "Film already exists"});
+    }
+
+    const nextId = films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
+
+    const addFilm: Film = { id: nextId, ...NewFilm };
+
+    films.push(addFilm);
+    return res.json(addFilm);
+}
+
+    const updateFilm = { ...films[index], ...body } as Film;
+
+    films[index] = updateFilm;
+
+    return res.send(updateFilm);
+})
+
+
 export default router;
