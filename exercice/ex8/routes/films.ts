bd0a1bea8980565e2e85a9ab43f290c869061659
Keyvan <keyvan.nikazm@gmail.com>
2024-10-08 10:04:23 +0200

    new: ex1.8

--- exercice/ex8/routes/films.ts
+++ exercice/ex8/routes/films.ts
@@ -1,57 +1,40 @@
 import {Router} from "express";
-import path from "node:path";
-import {Film, NewFilm} from "../types";
-import { parse, serialize } from "../utils/json";
-const jsonPath = path.resolve(__dirname, "/../data/films.json");
+import {NewFilm} from "../types";
+import {containsOnlyExpectedKeys} from "../utils/validate";
 
-const films : Film[] = [
-    {
-        id : 1,
-        title : "Star Wars",
-        director : "George Lucas",
-        duration : 121,
-    },
-
-    {
-        id : 2,
-        title : "Indiana Jones",
-        director : "Steven Spielberg",
-        duration : 115,
-    },
-
-    {
-        id : 3,
-        title : "Jurassic Park",
-        director : "Steven Spielberg",
-        duration : 127,
-    },
-];
+import {
+    readAllFilms,
+    ReadOneFilm,
+    CreateFilm,
+    DeleteFilm,
+    PatchFilm,
+} from "../services/films";
 
 const router = Router();
 
+const expectedKey = ["title", "director", "duration", "budget", "description", "imageUrl"];
+
 router.get("/:id", (req, res) => {
     const id = Number(req.params.id);
-    const Films = parse(jsonPath, films);
-    const film = Films.find((film) => film.id === id);
+    if (isNaN(id)) {
+        return res.sendStatus(400);
+    }
+    const film = ReadOneFilm(id);
     if (!film) {
-        return res.status(404).json("Film not found");
+        return res.sendStatus(404);
     }
     return res.json(film);
 });
 
 router.get("/", (req, res) => {
-    const film = parse(jsonPath, films);
-    if(!req.query["minimum-duration"]) {
-        return res.json(film);
+    const minDuration = "minimumduration" in req.query ? Number(req.query.minimumduration) : undefined;
+
+    if(minDuration && (typeof minDuration !== "number" || minDuration <= 0)){
+        return res.sendStatus(400);
     }
-    const minDuration = Number(req.query["minimum-duration"]);
-    if (isNaN(minDuration) || minDuration <= 0) {
-        return res.status(400).json("Invalid minimum-duration");
-    }
-    const filterFilms = film.filter((film) => {
-        return film.duration >= minDuration;
-    });
-    return res.json(filterFilms);
+
+    const Films = readAllFilms(minDuration);
+    return res.send(Films);
 });
 
 router.post("/", (req, res) => {
@@ -73,55 +56,31 @@ router.post("/", (req, res) => {
     ) {
         return res.sendStatus(400).json({error: "Invalid body"});
     }
-    const {title, director, duration, budget, description, imageUrl} = body as NewFilm;
+    const newFilm = body as NewFilm;
 
-    const Films = parse(jsonPath, films);
-
-    const nextId = Films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
-
-    const NewFilm: Film = {
-        id: nextId,
-        title,
-        director,
-        duration,
-        budget,
-        description,
-        imageUrl,
-    };
-
-    const exists = films.find(
-        (film) =>
-        (film.title.toLowerCase() === NewFilm.title.toLowerCase()) &&
-        (film.director.toLowerCase() === NewFilm.director.toLowerCase())
-    );
-
-    if (exists) {
-        return res.status(409).json({error: "Film already exists"});
+    const NewFilm = CreateFilm(newFilm);
+    if (!NewFilm) {
+        return res.sendStatus(409);
     }
-
-    Films.push(NewFilm);
-    serialize(jsonPath, Films);
     return res.json(NewFilm);
     });
 
 router.delete("/:id", (req,res) => {
     const id = Number(req.params.id);
-    const Films = parse(jsonPath, films);
-    const index = Films.findIndex((film) => film.id === id);
-    if (index === -1) {
-        return res.sendStatus(404).json({error: "Film not found"});
+    if(isNaN(id)){
+        return res.sendStatus(400);
     }
-    const deleteElements = Films.splice(index, 1);
-    serialize(jsonPath, Films);
-    return res.json(deleteElements[0]);
+    const deleteElements = DeleteFilm(id);
+    if(!deleteElements){
+        return res.status(404);
+    }
+    return res.send(deleteElements);
 });
 
 router.patch("/:id", (req, res) => {
     const id = Number(req.params.id);
-    const Films = parse(jsonPath, films);
-    const index = Films.findIndex((film) => film.id === id);
-    if (!index) {
-        return res.status(404);
+    if (isNaN(id)) {
+        return res.sendStatus(400);
     }
     const body: unknown = req.body;
 
@@ -134,90 +93,21 @@ router.patch("/:id", (req, res) => {
         ("description" in body && (typeof body.description !== "string")) ||
         ("imageUrl" in body && (typeof body.imageUrl !== "string"))
     ) {
-        return res.sendStatus(400).json({error: "Invalid body"});
+        return res.sendStatus(400);
     }
 
-    const { title, director, duration, budget, description, imageUrl }: Partial<NewFilm> = body;
-
-    if (title) {
-        films[index].title = title;
-    }
-    if (director) {
-        films[index].director = director;
-    }
-    if (duration) {
-        films[index].duration = duration;
-    }
-    if (budget) {
-        films[index].budget = budget;
-    }
-    if (description) {
-        films[index].description = description;
-    }
-    if (imageUrl) {
-        films[index].imageUrl = imageUrl;
+    if(!containsOnlyExpectedKeys(body, expectedKey)){
+        return res.sendStatus(400);
     }
 
-    serialize(jsonPath, films); 
+    const updateFilm = PatchFilm(id, body);
 
-    return res.json(films[index]);
-});
-
-// faire le router.put
-
-router.put("/:id", (req, res) => {
-    const body: unknown = req.body;
-    
-    if (!body || 
-        typeof body !== "object" ||
-        !("title" in body) ||
-        !("director" in body) ||
-        !("duration" in body) ||
-        typeof body.title !== "string" ||
-        typeof body.director !== "string" ||
-        typeof body.duration !== "number" ||
-        !body.title.trim() ||
-        !body.director.trim() ||
-        body.duration <= 0 ||
-        (("budget" in body) && (typeof body.budget !== "number" || body.budget <= 0)) ||
-        (("description" in body) && (typeof body.description !== "string")) ||
-        (("imageUrl" in body) && (typeof body.imageUrl !== "string"))
-    ) {
-        return res.sendStatus(400).json({error: "Invalid body"});
+    if (!updateFilm) {
+        return res.sendStatus(404);
     }
-
-    const id = Number(req.params.id);
-    if(!id){
-        return res.status(400);
-    }
-    const index = films.findIndex(films => films.id === id);
-    if(index < 0){
-        const NewFilm = body as NewFilm;
-    
-        const exists = films.find(
-            (film) =>
-            (film.title.toLowerCase() === NewFilm.title.toLowerCase()) &&
-            (film.director.toLowerCase() === NewFilm.director.toLowerCase())
-        );
-
-    if (exists) {
-        return res.status(409).json({error: "Film already exists"});
-    }
-
-    const nextId = films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
-
-    const addFilm: Film = { id: nextId, ...NewFilm };
-
-    films.push(addFilm);
-    return res.json(addFilm);
-}
-
-    const updateFilm = { ...films[index], ...body } as Film;
-
-    films[index] = updateFilm;
 
     return res.send(updateFilm);
-})
+});
 
 
 export default router;
