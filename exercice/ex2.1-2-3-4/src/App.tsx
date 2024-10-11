b4ced484bbe49e7f760452181a48d3116cbd973a
Keyvan <keyvan.nikazm@gmail.com>
2024-10-11 12:57:13 +0200

    new:ex2.1

--- exercice/ex2.1-2-3-4/src/App.tsx
+++ exercice/ex2.1-2-3-4/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+      <Page title="Informations sur les films dans les cinémas"/> 
+      <Cinema name="UGC DeBrouckère" title1="Film 1 - DeBrouckère" director1="Director A" title2="Film 2 - DeBrouckère" director2="Director B"/>
+      <Cinema name="UGC Toison d'Or" title1="Film 1 - Toison d'Or" director1="Director C" title2="Film 2 - Toison d'Or" director2="Director D"/>
+    </div>
+  );
+}
+
+const Page = (props : {title:string}) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
   )
 }
 
-export default App
+const Cinema = (props : {name:string, title1:string, director1:string, title2:string, director2:string}) => {
+  return(
+    <div>
+        <h2>{props.name}</h2>
+        <ul>
+          <li>
+            <strong>{props.title1}</strong> - Réalisateur :{" "}
+            {props.director1}
+          </li>
+          <li>
+            <strong>{props.title2}</strong> - Réalisateur :{" "}
+            {props.director2}
+          </li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
